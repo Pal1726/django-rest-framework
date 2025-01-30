ba776baa2c9e724196ceeea86abca73e6d28f934
Palak <palakrathiiit@gmail.com>
2025-01-30 18:50:04 +0530

    added

--- snippets/views.py
+++ snippets/views.py
@@ -202,6 +202,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import viewsets
+from rest_framework import permissions
+
+from rest_framework.decorators import action
+
 
 @api_view(['GET'])
 def api_root(request, format=None):
